a19045a0c8208c71fb5892a34fff06ed3b0ade49
Jonathan Hudak <hudak.jonathan@gmail.com>
2019-03-06 21:59:13 -0800

    Updates

---
+++ precache-manifest.adb70a054e57ae0494c7a4fe222e0616.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "986837b8a9d5a1f7926d",
+    "url": "/project-manager-application/static/css/main.ef646a55.chunk.css"
+  },
+  {
+    "revision": "986837b8a9d5a1f7926d",
+    "url": "/project-manager-application/static/js/main.c456e786.chunk.js"
+  },
+  {
+    "revision": "78dbd8c633a92e8b5989",
+    "url": "/project-manager-application/static/js/runtime~main.972b3edd.js"
+  },
+  {
+    "revision": "3a9777c472bba6639626",
+    "url": "/project-manager-application/static/js/2.3ef82dbf.chunk.js"
+  },
+  {
+    "revision": "fb9f57541d31b3d1b5d6ecb4dfa86456",
+    "url": "/project-manager-application/index.html"
+  }
+];
