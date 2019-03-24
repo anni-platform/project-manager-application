6c2f237733fd58f53d3b39792bb74e53d5d59bd4
Jonathan Hudak <hudak.jonathan@gmail.com>
2019-03-24 09:44:49 -0700

    Load image sequence with canvas-image-scrubber, WIP - buggy

--- src/project/sections/Animation/index.js
+++ src/project/sections/Animation/index.js
@@ -1,141 +1,14 @@
-import React, { useEffect, useState, useReducer } from 'react';
-import isEqual from 'lodash/isEqual';
+import React, { Suspense } from 'react';
 import Loader from 'shared/Loader';
-import { useDropboxClient } from 'shared';
 
-function FolderPicker({ onSelectFolder, selectedFolderId }) {
-  const [folders, setFolders] = useState(null);
-  const { client } = useDropboxClient();
+const AnimationSection = React.lazy(() => import('./AnimationSection'));
 
-  useEffect(() => {
-    async function listFiles() {
-      const result = await client.filesListFolder({
-        path: '',
-      });
-      const folders = result.entries.filter(
-        e => e['.tag'] === 'folder' && e.name !== 'images'
-      );
-      setFolders(folders);
-    }
-    listFiles();
-  }, []);
-
-  return folders === null ? (
-    <Loader />
-  ) : (
-    <select
-      onChange={({ target }) =>
-        onSelectFolder(folders.find(f => f.id === target.value))
-      }
-      value={selectedFolderId}
-    >
-      <option value={undefined}>
-        {' '}
-        -- select an animation folder or video file --{' '}
-      </option>
-      {folders.map(({ id, name }) => (
-        <option key={id} value={id}>
-          {name}
-        </option>
-      ))}
-    </select>
-  );
-}
-
-function useListFolderFiles(folder, onReceiveFolderFiles) {
-  const [files, setItems] = useState(null);
-  const { client } = useDropboxClient();
-
-  useEffect(
-    () => {
-      async function listFiles() {
-        const result = await client.filesListFolder({
-          path: folder.path_display,
-        });
-        const files = await Promise.all(
-          result.entries.map(async f => {
-            const file = await client.sharingCreateSharedLink({
-              path: f.path_display,
-            });
-            return {
-              ...f,
-              ...file,
-              index: parseInt(/\d+/g.exec(f.name)[0], 10),
-              url: file.url.replace(/.$/, '1'),
-            };
-          })
-        );
-        const sortedFiles = files.sort((a, b) => a.index - b.index);
-        setItems(sortedFiles);
-      }
-      if (folder) listFiles();
-    },
-    [folder]
-  );
-
-  useEffect(
-    () => {
-      onReceiveFolderFiles(files);
-    },
-    [files]
-  );
-}
-
-const reducer = (state, nextState) => ({ ...state, ...nextState });
-
-export default function AnimationSection({
-  save,
-  selectedFolder: defaultSelectedFolder,
-  files: defaultFiles,
-  ...restProps
-}) {
-  const defaultSettings = {
-    selectedFolder: defaultSelectedFolder,
-    files: defaultFiles,
-  };
-  const [isSaveDisabled, setIsSaveDisabled] = useState(true);
-  const [settings, updateSettings] = useReducer(reducer, defaultSettings);
-  const { selectedFolder, files } = settings;
-  useListFolderFiles(selectedFolder || defaultSelectedFolder, files =>
-    updateSettings({ files })
-  );
-
-  useEffect(
-    () => {
-      setIsSaveDisabled(isEqual(defaultSettings, settings));
-    },
-    [settings]
-  );
-
+export default function ProjectProseSection(props) {
   return (
     <div>
-      <FolderPicker
-        onSelectFolder={selectedFolder =>
-          updateSettings({ selectedFolder, files: null })
-        }
-        selectedFolderId={
-          selectedFolder ? selectedFolder.id : defaultSelectedFolder
-        }
-      />
-      {selectedFolder &&
-        (Array.isArray(files) ? (
-          <ul style={{ display: 'flex', flexWrap: 'wrap', listStyle: 'none' }}>
-            {files.map(f => (
-              <li key={f.id}>
-                <img src={f.url} width={100} alt={f.name} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <Loader />
-        ))}
-      <button
-        disabled={isSaveDisabled}
-        onClick={() => save(settings)}
-        title={isSaveDisabled ? 'No changes to save' : null}
-      >
-        Save animation
-      </button>
+      <Suspense fallback={<Loader />}>
+        <AnimationSection {...props} />
+      </Suspense>
     </div>
   );
 }
